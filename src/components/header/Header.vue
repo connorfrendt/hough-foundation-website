73a2f2a57b82193343be1392781dea376a05728e
Connor Frendt <42262048+connorfrendt@users.noreply.github.com>
2026-03-11 16:49:33 -0700

    menu is toggleable

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,28 +1,39 @@
 <template>
     <div>
         <header>
-            <IconHamburgerList></IconHamburgerList>
-            <div>Home</div>
-            <div>About Us</div>
-            <div>Programs</div>
-            <div>Services</div>
-            <div>Get Involved</div>
-            <div>Events</div>
-            <div>Stories & News</div>
-            <div>Donate</div>
-            <div>Smart Giving</div>
-            <div>Contact Us</div>
-            <div>Get Engaged</div>
-            <div>Volunteer</div>
-            <div>Subscribe to Newsletter</div>
+            <div class="menu-opener" @click="menuOpener">
+                <IconHamburgerList></IconHamburgerList>
+            </div>
+            <div class="menu-wrapper" v-show="isMenuOpen">
+                <div>Home</div>
+                <div>About Us</div>
+                <div>Programs</div>
+                <div>Services</div>
+                <div>Get Involved</div>
+                <div>Events</div>
+                <div>Stories & News</div>
+                <div>Donate</div>
+                <div>Smart Giving</div>
+                <div>Contact Us</div>
+                <div>Get Engaged</div>
+                <div>Volunteer</div>
+                <div>Subscribe to Newsletter</div>
+            </div>
         </header>
     </div>
 </template>
 
 <script setup>
 import IconHamburgerList from '@/icons/IconHamburgerList.vue';
+import { ref } from 'vue';
 
+const isMenuOpen = ref(false);
 
+function menuOpener() {
+    console.log('Menu Open? ', isMenuOpen.value);
+    isMenuOpen.value = !isMenuOpen.value;
+    console.log('Menu Open? ', isMenuOpen.value);
+}
 </script>
 
 <style scoped lang="scss" src="./header.scss"></style>
