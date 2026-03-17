3c102f45af1d72a513facd997708760c1ce25145
Connor Frendt <42262048+connorfrendt@users.noreply.github.com>
2026-03-17 10:40:54 -0700

    working on about us and header

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -8,14 +8,15 @@
                 <div>Vancouver, WA 98660</div>
                 <div>(360) 992-7060</div>
             </div>
-        </div>
-        <div class="tax-info">
-            <div>Registered 501(c)(3)</div>
-            <div>Tax ID: 94-3160811</div>
+            <div class="tax-info">
+                <!-- <div>Registered 501(c)(3)</div>
+                <div>Tax ID: 94-3160811</div> -->
+                <div>© 2026 Hough Foundation is a registered 501(c)(3) non-profit.</div>
+            </div>
         </div>
         <div class="connect">
-            <div>Connect With Us!</div>
-            <IconFacebook class="fdsa"></IconFacebook>
+            <h4>Connect With Us!</h4>
+            <IconFacebook class="icon-facebook"></IconFacebook>
         </div>
     </div>
 </template>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
-        <header>
+
+        <!--MOBILE HEADER-->
+        <header class="mobile-header">
             <div class="upper-header-wrapper">
                 <h1>
-                    <href url="about:blank" target="_blank">Hough Foundation</href>
+                    <a href="about:blank" target="_blank">Hough Foundation</a>
                 </h1>
                 <div class="menu-opener">
                     <IconHamburgerList @click="menuOpener"></IconHamburgerList>
                 </div>
             </div>
             <div class="menu-wrapper" v-show="isMenuOpen">
+                <div><RouterLink class="info-bar-button" @click="closeMenu" to="/">Home</RouterLink></div>
+                <div><RouterLink class="info-bar-button" @click="closeMenu" to="/about-us">About Us</RouterLink></div>
+                <div>Programs</div>
+                <div>Services</div>
+                <div>Get Involved</div>
+                <div>Events</div>
+                <div>Stories & News</div>
+                <div>Donate</div>
+                <div>Smart Giving</div>
+                <div>Contact Us</div>
+                <div>Get Engaged</div>
+                <div>Volunteer</div>
+                <div>Subscribe to Newsletter</div>
+            </div>
+        </header>
+
+        <!--DESKTOP HEADER-->
+        <header class="desktop-header">
+            <div>
+                <h1>
+                    <href url="about:blank" target="_blank">Hough Foundation</href>
+                </h1>
+            </div>
+            <div class="menu-wrapper">
                 <div><RouterLink class="info-bar-button" @click="closeMenu" to="/">Home</RouterLink></div>
                 <div><RouterLink class="info-bar-button" @click="closeMenu" to="/about-us">About Us</RouterLink></div>
                 <div>Programs</div>
