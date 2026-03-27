dcc658af032a075595bdc908328cfbeab832e8dc
Connor Frendt <42262048+connorfrendt@users.noreply.github.com>
2026-03-27 12:50:03 -0700

    wip

--- src/components/gifts-stock-qcd/GiftsStockQCD.vue
+++ src/components/gifts-stock-qcd/GiftsStockQCD.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        Gifts Here
+        <header>Gifts of Stock and QCD</header>
+        <div>Gifts of stock or a gift from your IRA (also known as Qualified Charitable Distribution (QCD)) make a big impact for the animals and also have some great benefits for you.  Learn more about gifts of stock below or click here for IRA gifts and QCD.</div>
     </div>
 </template>
 
